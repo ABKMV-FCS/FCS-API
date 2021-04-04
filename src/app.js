0af754d2040e4f2eaea401ac730a777a673c924b
Sai Kiran <saikiran9620@gmail.com>
2021-04-05 01:19:12 +0530

    removed security hotspots

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { query, connect } = require('./db');
-const cors = require('cors');
 const morgan = require('morgan');
 const app = express();
+app.disable("x-powered-by");
 
 
-
-app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
