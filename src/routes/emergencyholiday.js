6b3eb0c7dbe53313b1cf60656139fe76e6616c18
Venkat <venkatasubramanian2001@gmail.com>
2021-05-02 16:51:22 +0530

    emergency holiday

---
+++ src/routes/emergencyholiday.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const { query } = require('../db');
+
+router.post('/createnewholiday', async (req, res) => {
+    let { date, description } = req.body;
+    try {
+      await query(`insert into holiday values ('${date}','${reason}');`);
+        await query(`delete from calendar where date like '${date}';`);
+        return res.status(200).json({ message: 'deletion successful' });
+
+    } catch (error) {
+      return res.status(500).json({ message: error });
+    }
+  });
