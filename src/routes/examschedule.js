5183d30ea259fa5c38a306a2cb9f7deb1fa4bbad
Sai Kiran <saikiran9620@gmail.com>
2021-05-31 22:38:12 +0530

    examscheduleinit subroute added

--- src/routes/examschedule.js
+++ src/routes/examschedule.js
@@ -12,11 +12,12 @@ router.post('/examscheduleinit', async (req, res) => {
     await query(`delete from calendar where sem like '${sem}' and academic_year like '${academic_year}' and date>='${startdate}' and date<='${enddate}';`)
     for(row of es){
       await query(`insert into exam_slot values('${row[0]}','${row[1]}','${row[2]}','${row[3]}','${row[4]}','${row[5]}','${row[6]}','${academic_year}');`)
-      await query(`delete from calendar c where (c.coursecode, c.slot,c.dept,c.class,c.sem) IN (select coursecode,  slot,dept,section,sem from timetable t where t.day= '${moment(row[0], "YYYY-MM-DD").format('dddd')}' and (t.slot >= '${config["exam_slots"][row[1]]["periods"][0]}' and t.slot <= '${config["exam_slots"][row[1]]["periods"][len-1]}')) and (t.dept, t.coursecode,t.section,t.sem) IN (select dept, coursecode,section,sem from faculty_subject fs where fs.sem <> '${row[3]}' and fs.faculty is like '${row[5]}');`)
-      
+      row[1] = "et"+ row[1]
+      let day = moment(row[0], "YYYY-MM-DD").format('dddd');
+      await query(`delete from calendar c where (c.coursecode, c.slot,c.dept,c.section,c.sem) IN (select coursecode,  slot,dept,section,sem from timetable t where t.day= '${day}' and (t.slot >= '${config["exam_slots"][row[1]]["periods"][0]}' and t.slot <= '${config["exam_slots"][row[1]]["periods"][config["exam_slots"][row[1]]["periods"].length-1]}')) and (dept, coursecode,section,sem) IN (select dept, coursecode,section,sem from faculty_subject fs where fs.sem <> '${row[3]}' and fs.faculty like '${row[5]}');`)
       let sections = await query(`select * from dept_class where dept like '${row[4]}'`)
       for(section of sections){
-        await query(`insert into calendar values('${row[0]}','${slot}','${row[2]}','${section['section']}','${row[4]}','${row[3]}','${academic_year}'`)
+        await query(`insert into calendar values('${row[0]}','${row[1]}','${row[2]}','${section['section']}','${row[4]}','${row[3]}','${academic_year}');`)
       }
     }
     return res.status(200).json({ message: 'Update Successful' });
