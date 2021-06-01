523beb28c0c7a8f1d6e0bb83f27a5d5b9ebf6c3d
Monish Raaj L <monishraaj05@gmail.com>
2021-06-01 20:02:53 +0530

    student subscription done

--- src/routes/student.js
+++ src/routes/student.js
@@ -13,14 +13,36 @@ router.get('/subscribeinit', async (req, res) => {
 			if (!departmentClasses[row.dept]) departmentClasses[row.dept] = [];
 			departmentClasses[row.dept].push(row.section);
 		}
-		
-		return res.status(200).json({ academic_year, odd, departmentClasses});
+
+		return res.status(200).json({ academic_year, odd, departmentClasses });
 	}
 	catch (error) {
 		console.log(error);
 		return res.status(500).json({ message: error });
 	}
 });
+
+router.post('/subscribe', async (req, res) => {
+	try {
+		let {
+			name,
+			email,
+			sem,
+			dept,
+			section,
+			academic_year,
+			fcmToken
+		} = req.body;
+		await query(`insert into student_subscribe values('${email}','${name}','${dept}','${section}',${sem},${academic_year},'${fcmToken}') ON DUPLICATE KEY UPDATE name='${name}', dept='${dept}', section='${section}', sem='${sem}', academic_year='${academic_year}', fcmToken='${fcmToken}'`)
+		return res.status(200).json({ message:'subscribed successfully!'});
+	}
+	catch (error) {
+		console.log(error);
+		return res.status(500).json({ message: error });
+	}
+});
+
+
 
 
 
