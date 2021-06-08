506cfb7d972d5a91e5e4879bba69b288d936b3c8
Monish Raaj L <monishraaj05@gmail.com>
2021-06-09 01:33:44 +0530

    not stopping app on sql connection error

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ console.log(`connecting to SQL Server...`);
 connect(function (err) {
 	if(err){
 		console.log(`Cannot connect to SQL Server`);
-		process.exit(1);
+		// process.exit(1);
 	}
 	else{
 			console.log('successfully connected to SQL server');
