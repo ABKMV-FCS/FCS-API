e8a295767ebfbb2f802353c9a8734d9e28dbba0e
Sai Kiran <saikiran9620@gmail.com>
2021-06-09 19:10:55 +0530

    jasmine - student test added

--- spec/student/student_test.js
+++ spec/student/student_test.js
@@ -1,21 +1,16 @@
-require('mocha');
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-chai.use(chaiHttp);
-const should = chai.should();
 const test_config = require('../test_config');
-
-
-
+var Request = require('request')
 describe('Student Routes Check', () => {
-
     it('subscribe init', (done) => {
-        chai.request(test_config.baseURL).get('/student/subscribeinit').end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.have.property('academic_year');
-            res.body.should.have.property('odd');
-            res.body.should.have.property('departmentClasses');
+        let options = {
+            url: `${test_config.baseURL}/student/subscribeinit`,
+        };
+        Request.get(options, (err, res) => {
+            expect(res.statusCode).toBe(200)
+            expect(res.body).toContain('academic_year');
+            expect(res.body).toContain('odd');
+            expect(res.body).toContain('departmentClasses');
             done();
-        });
+        })
     });
 });
